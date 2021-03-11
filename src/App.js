630ec8dbada94998ed77640a67c189c2a306bf20
Royce Townsend <git@roycetownsend.com>
2021-03-11 21:55:48 +0800

    Scaffold of Graph component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+
+function Graph(props) {
+  const [graphLoaded, setGraphLoaded] = useState(false);
+
+  // load graph
+  // https://medium.com/swlh/how-to-use-amcharts-4-with-react-hooks-999a62c185a5
+
+  const showNoData = !props.loading && props.data.length === 0;
+  const showGraph = !props.loading && props.data.length > 0 && graphLoaded;
+  const showLoading = props.loading || (props.data.length > 0 && !graphLoaded);
+
+  return (
+    <div>
+      {showLoading && <div>SPINNER</div>}
+      {showNoData && <div>NO DATA</div>}
+
+      <div id={props.id}></div>
+    </div>
+  );
+}
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setTimeout(() => setData([]), 1000);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Graph loading={!data} data={data} />
       </header>
     </div>
   );
